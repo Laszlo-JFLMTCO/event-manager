6bfa90008363ba4961a172902f789afb45326408
Laszlo Balogh <laszlo@thebaloghs.com>
2016-10-30 13:03:08 -0600

    Iteration4 with ERB-based form letters

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -9,12 +9,18 @@ include Output
 
 puts "EventManager initialized"
 
+# path = "./data/event_attendeesONEENTRY.csv"
 path = "./data/event_attendees.csv"
+
+path_letter_template = "./data/letter_template.erb"
 
 puts "Through enumerable as object"
 CSV.open(path, headers:true, header_converters: :symbol) do |people|
   person = people.each
-  person.select do |item|
-    puts printout(item)
+  puts "Printing letters..."
+  person.select do |one_person|
+    printout(one_person, path_letter_template)
   end
 end
+
+puts "All done! :-)"
--- lib/output.rb
+++ lib/output.rb
@@ -1,20 +1,47 @@
 require 'pry'
 require_relative 'filters'
+require 'erb'
+
 module Output
   include Filters
+
+  def legislator_list_formatted(legislators)
+    legislators.map do |one_legislator|
+      "#{one_legislator.first_name} #{one_legislator.last_name}"
+    end.join(", ")
+  end
 
   def legislator_list(zipcode)
     Sunlight::Congress.api_key = "e179a6973728c4dd3fb1204283aaccb5"
 
     legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
-      legislators.map do |one_legislator|
-        "#{one_legislator.first_name} #{one_legislator.last_name}"
-      end.join(", ")
   end
 
-  def printout(row)
-    zipcode = zip_fixer(row[:zipcode])
-    "#{row[:first_name]} #{zipcode} [#{legislator_list(zipcode)}]"
+  def printout(one_person, path)
+    zipcode = zip_fixer(one_person[:zipcode])
+    legislators = legislator_list(zipcode)
+    "#{one_person[:first_name]} #{zipcode} [#{legislator_list_formatted(legislators)}]"
+    custom_letter(path, one_person, zipcode, legislators)
   end
 
+  def custom_letter(path, one_person, zip_code, legislators)
+    letter_template = load_template(path) 
+    name = one_person[:first_name]
+    output = letter_template.result(binding)
+    id = one_person[0]
+    save_letter(output, id)
+  end
+
+  def load_template(path)
+    loading_template = File.read(path) if File.exist?(path)
+    ERB.new(loading_template)
+  end
+
+  def save_letter(output, id)
+    path = "./data/output/"
+    filename = "#{path}#{id}_letter.html"
+    File.open(filename, 'w') do |file|
+      file.puts output
+    end
+  end
 end
